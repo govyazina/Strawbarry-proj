652323a02ac9a68806c31fd419eba80ebefceb4c
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-06-09 17:10:11 +0300

    feat: create cart.module.scss and import styles for cart

--- online_store/src/views/Cart/Cart.jsx
+++ online_store/src/views/Cart/Cart.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import SelectedBouquets from '../../components/SelectedBouquets/SelectedBouquets';
 import OrderForm from '../../components/OrderForm/OrderForm';
+import styles from './cart.module.scss';
 
 function Cart() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <SelectedBouquets />
       <OrderForm />
-    </>
+    </div>
   );
 }
 
