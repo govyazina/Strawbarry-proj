92138877c99e4f3d982f2355604dc575089b2635
Svetlana Govyazina <govyazina@gmail.com>
2023-06-09 18:06:36 +0300

    init new components (product filter and list)

--- online_store/src/views/Main/Main.jsx
+++ online_store/src/views/Main/Main.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { TimePicker } from 'antd';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import dayjs from 'dayjs';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import Title from 'antd/es/typography/Title';
+import ProductFilter from '../ProductFilter/ProductFilter';
+import ProductList from '../ProductList/ProductList';
 
 export default function Main() {
-  dayjs.extend(customParseFormat);
-  const onChange = (time, timeString) => {
-    // eslint-disable-next-line no-console
-    console.log(time, timeString);
-  };
-
   return (
-    <TimePicker onChange={onChange} defaultOpenValue={dayjs('00:00:00', 'HH:mm:ss')} />
+    <>
+      <Title>Букеты из клубники</Title>
+      <ProductFilter />
+      <ProductList />
+    </>
   );
 }
