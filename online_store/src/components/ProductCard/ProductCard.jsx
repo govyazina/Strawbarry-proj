5456a37a642ec8ab20aef75c1b34ab420613da2d
Svetlana Govyazina <govyazina@gmail.com>
2023-06-12 14:25:07 +0300

    added navigate to buket-id-page

--- online_store/src/components/ProductCard/ProductCard.jsx
+++ online_store/src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Button, Card, Space } from 'antd';
 import Meta from 'antd/es/card/Meta';
+import { useNavigate } from 'react-router-dom';
 import styles from './productcard.module.scss';
 
 export default function ProductCard({ product }) {
+  const navigate = useNavigate();
+  const id = product.sku;
+  const clickHandler = () => navigate(`/bouquet/${id}`);
   return (
     <Card
+      onClick={clickHandler}
       hoverable
       bordered={false}
       style={{
