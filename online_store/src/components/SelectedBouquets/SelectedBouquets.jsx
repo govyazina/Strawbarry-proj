9e0ce07427eea8f17995bb6329a6bf828e87783b
Irina <a98659193868@gmail.com>
2023-06-15 00:37:59 -0400

    partially

--- online_store/src/components/SelectedBouquets/SelectedBouquets.jsx
+++ online_store/src/components/SelectedBouquets/SelectedBouquets.jsx
@@ -1,124 +1,159 @@
-import React from 'react';
-import useProductList from '../../hooks/useProductList';
+import React, { useEffect, useState } from 'react';
+// import useProductList from '../../hooks/useProductList';
 import styles from './selectedBouquets.module.scss';
-import { Link } from 'react-router-dom';
 import Order from '../../views/Order/Order';
-import {Table, Row, Col, Image} from 'antd'
 
 
 
 function SelectedBouquets() {
 
-//   const dataSource = [
-//     {
-// name:	"new_york",
-// name_title:	"Букет Нью-Йорк S",
-// sku:	41186,
-// type:	"bouquet",
-// size:	"S",
-// price:	1799,
-// description:	
-// {
-// title:	"Букет из клубники в шоколаде",
-// gift_size:	"ширина 16см х высота 22см",
-// ingredients:	"Свежая клубника в бельгийском шоколаде - 10 шт, клубника без шоколада- 6 шт",
-// product_details: {
-// berries: {	
-// 0:	"клубника"
-// },
-// chocolate:
-// {	
-// 0:	"молочный"
-// },
-// topping:	"малина сублимированная, кокосовая стружка"
-// },
-// photos: {
-// 0:	"http://drive.google.com/uc?export=view&id=1WxIjt2Honsm2A9TUX0TSojukLRs1s7is",
-// 1:	"http://drive.google.com/uc?export=view&id=1CUdv4muNXn32p_kHAflbw6KHJdsEQ9e9",
-// 2:	"http://drive.google.com/uc?export=view&id=1qo6DNn2-rCkuyyPY14v0ORgRDRmz8-N8",
-// }
-// }
-//       },
-// {
-//   name:	"new_york",
-//   name_title:	"Букет Нью-Йорк S",
-//   sku:	41186,
-//   type:	"bouquet",
-//   size:	"S",
-//   price:	1799,
-//   description:	
-//   {
-//   title:	"Букет из клубники в шоколаде",
-//   gift_size:	"ширина 16см х высота 22см",
-//   ingredients:	"Свежая клубника в бельгийском шоколаде - 10 шт, клубника без шоколада- 6 шт",
-//   product_details: {
-//   berries: {	
-//   0:	"клубника"
-//   },
-//   chocolate:
-//   {	
-//   0:	"молочный"
-//   },
-//   topping:	"малина сублимированная, кокосовая стружка"
-//   },
-//   photos: {
-//   0:	"http://drive.google.com/uc?export=view&id=1WxIjt2Honsm2A9TUX0TSojukLRs1s7is",
-//   1:	"http://drive.google.com/uc?export=view&id=1CUdv4muNXn32p_kHAflbw6KHJdsEQ9e9",
-//   2:	"http://drive.google.com/uc?export=view&id=1qo6DNn2-rCkuyyPY14v0ORgRDRmz8-N8",
-//   }
-//   }
-// }
-//   ]
+  const cartData = [
+    {
+name:	"new_york",
+name_title:	"Букет Нью-Йорк S",
+sku:	41186,
+type:	"bouquet",
+size:	"S",
+price:	1799,
+count: 1,
+description:	
+{
+title:	"Букет из клубники в шоколаде",
+gift_size:	"ширина 16см х высота 22см",
+ingredients:	"Свежая клубника в бельгийском шоколаде - 10 шт, клубника без шоколада- 6 шт",
+product_details: {
+berries: {	
+0:	"клубника"
+},
+chocolate:
+{	
+0:	"молочный"
+},
+topping:	"малина сублимированная, кокосовая стружка"
+},
+photos: {
+0:	"http://drive.google.com/uc?export=view&id=1WxIjt2Honsm2A9TUX0TSojukLRs1s7is",
+1:	"http://drive.google.com/uc?export=view&id=1CUdv4muNXn32p_kHAflbw6KHJdsEQ9e9",
+2:	"http://drive.google.com/uc?export=view&id=1qo6DNn2-rCkuyyPY14v0ORgRDRmz8-N8",
+}
+}
+      },
+{
+  name:	"new_york",
+  name_title:	"Букет Нью-Йорк S",
+  sku:	41186,
+  type:	"bouquet",
+  size:	"S",
+  price:	1799,
+  count: 2,
+  description:	
+  {
+  title:	"Букет из клубники в шоколаде",
+  gift_size:	"ширина 16см х высота 22см",
+  ingredients:	"Свежая клубника в бельгийском шоколаде - 10 шт, клубника без шоколада- 6 шт",
+  product_details: {
+  berries: {	
+  0:	"клубника"
+  },
+  chocolate:
+  {	
+  0:	"молочный"
+  },
+  topping:	"малина сублимированная, кокосовая стружка"
+  },
+  photos: {
+  0:	"http://drive.google.com/uc?export=view&id=1WxIjt2Honsm2A9TUX0TSojukLRs1s7is",
+  1:	"http://drive.google.com/uc?export=view&id=1CUdv4muNXn32p_kHAflbw6KHJdsEQ9e9",
+  2:	"http://drive.google.com/uc?export=view&id=1qo6DNn2-rCkuyyPY14v0ORgRDRmz8-N8",
+  }
+  }
+}
+  ]
 
-//   const columns = [
-//   {
-//     title: 'Фото букета',
-//     dataIndex: 'photo',
-//     key: 'photo',
-//     render: (photos) => <Image src={photos} alt="bouquet_photo" width={100} />
-//   },
-//     {
-//     title: 'Название',
-//     dataIndex: 'name_title',
-//     key: 'title',
-//     // render: (text) => <Link to={"/bouquet/${id}"}/>
-//     },
-//     {
-//       title: 'Описание',
-//       dataIndex: 'description',
-//       key: 'description'
-//     },
-//     {
-//       title: 'Количество',
-//       dataIndex: 'quantity',
-//       key: 'quantity'
-//     },
-//     {
-//       title: 'Стоимость',
-//       dataIndex: 'price',
-//       key: 'price'
-//     }
-// ]
+ const [cart, setCart] = useState(cartData)
+ const [total, setTotal] = useState({
+  price: cart.reduce((prev, curr) =>  prev + curr.price, 0),
+  count: cart.reduce((prev, curr) => prev + curr.count, 0)
+ })
+ 
+ useEffect(() => {
+  setTotal({
+    price: cart.reduce((prev, curr) => prev + curr.price, 0),
+    count: cart.reduce((prev, curr) => prev + curr.count, 0)
+  })
 
-  let sum = 0;
+ }, [cart])
+
+ const increase = () => {
+  setCart(cart => {
+    return cart.map((product) => {
+      if (product.id === id) {
+        return {
+          ...product,
+          count: ++product.count,
+          price: product.count*product.price
+        }
+      }
+      return product
+    })
+  })
+}
+
+const decrease = () => {
+  setCart(cart => {
+    return cart.map((product) => {
+      if (product.id === id) {
+        const newCount = product.count - 1 > 1 ? product.count - 1 : 1
+        return {
+          ...product,
+          count: newCount,
+          price: newCount * product.price 
+        }
+      }
+      return product
+    })
+  })
+}
+
+const changeValue = (id, value) => {
+      setCart(cart => {
+        return cart.map(product => {
+          if (product.id === id) {
+            return {
+              ...product,
+              count: value,
+              price: value * product.price
+            }
+          }
+          return product
+        })
+      })
+
+}
+
+
+ 
+ const products = cart.map(product =>{
+  return <Order product = {product} key = {product.id} increase={increase} decrease={decrease} changeValue={changeValue} />
+ })
+
+  // let sum = 0;
   // productList.forEah(el => sum += Number.parseFloat(el.price))
 
   return (
     <>
+     <h1 className={styles.cart__header}>Корзина</h1>
+          <div className={styles.table__header}>
+            <div>Наименование</div>
+            <div>Количество</div>
+            <div>Стоимость</div>
+          </div>
       <div className={styles.bouquetWrapper}>
-        {/* <Row>
-          <Col>
-            <Table
-              dataSource={dataSource}
-              columns={columns}
-          />
-          </Col>
-        </Row> */}
-        <Order />
+        {products}
       </div>
       <div className={styles.table__footer}>
         <div className={styles.sum}>Сумма заказa </div>
-        <div className={styles.summary}>{sum}</div>
+        <div className={styles.summary}>{total.price} рублей</div>
       </div>
       
     </>
--- online_store/src/views/Order/Order.jsx
+++ online_store/src/views/Order/Order.jsx
@@ -2,50 +2,52 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './order.module.scss';
 import { CloseOutlined, UpOutlined, DownOutlined } from '@ant-design/icons';
-// import useProductList from '../../hooks/useProductList';
-// import styles from './order.module.scss';
+import {deleteCartAC} from '../../store/actions/mainActions';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-// import {deleteCartAC} from '../../store/actions/mainActions';
-// import { useDispatch } from 'react-redux';
-// import { useParams } from 'react-router-dom';
-// import { useState } from 'react';
 
-function Order(props) {
+function Order({product, increase, decrease}) {
 
-  // const [quantity, setQuantity] = useState(1)
-
-  // const cart = productList
-
-  // const cartItem = cart.map(item => ({...productList.find(a => a.id), ...item}))
-
+  
+  
     // const {id} = useParams();
+    const dispatch = useDispatch();
+    const deleteCart = (id) => {
+        dispatch(deleteCartAC(id))
+        console.log('delete', id)
+    }
 
-    // const dispatch = useDispatch();
+    const { description, picture, price, count } = product;
 
-    // const deleteCart = (id) => {
-    //     dispatch(deleteCartAC(id))
-    // }
+    
+    const cartSummary = 0
+    function getSummary () {
+      cartData.map( el => {
+        if (el.id===id) {
+          cartSummary = el.price + el.topping.price
+        }
+        return cartSummary
+      })
+    }
 
     
 
     return (
         <>
-          <h1 className={styles.cart__header}>Корзина</h1>
-          <div className={styles.table__header}>
-            <div>Наименование</div>
-            <div>Количество</div>
-            <div>Стоимость</div>
-          </div>
+         
           <section className={styles.order__wrap}>
           <div className={styles.cart__body}>
             <div className={styles.bouquetPic}>
-              <img className={styles.cartPic} src='https://avatars.mds.yandex.net/i?id=61f45c2a30854ed7c31bcc4b2b8ab7ba0f7e3f97-9214528-images-thumbs&n=13' alt='choicePic' />
+              <img className={styles.cartPic} src='' alt='choicePic' />
               <div className={styles.bouquetView}>
               <Link to={"/bouquet/${id}"} className={styles.link}>
-                bouquet.name
+                {product.name_title}
               </Link>
               <p className={styles.addings}>
-                bouquet.description
+                <p> {description.title}</p>
+                {/* <p>Ягоды: {product.description.product_details.berries}</p> */}
+                <p>Топпинги: {description.product_details.topping}</p>
               </p>
             </div>
           </div>
@@ -53,29 +55,19 @@ function Order(props) {
           </div>
           
           <div className={styles.quantity}>
-            <input type='number' className={styles.count__input} min='1' value='1' />
+            <input type='number' className={styles.count__input} min='1' value={count} onChange={(e) => changeValue(id, +e.target.value)} />
             <div className={styles.control}>
-            <button className={styles.button__quantity}>
+            <button className={styles.button__quantity} onClick={() => increase(id)}>
               <UpOutlined />
             </button>
-            <button className={styles.button__quantity}>
+            <button className={styles.button__quantity} onClick={() => decrease(id)}>
               <DownOutlined />
             </button>
             </div>
-            {/* <button className={styles.button__quantity}
-            // onClick={setQuantity(id, -1)}
-            >-</button>
-            <p>quantity</p>
-            <button className={styles.button__quantity}
-            // onClick={setQuantity(id, +1)}
-            >+</button> */}
           </div>
-          <div className='price'>Стоимость</div>
+          <div className='price'>{price}</div>
           <div className='delete'>
-            <CloseOutlined />
-            {/* <button 
-              // onClick={() => deleteCart(id)}
-              >delete</button> */}
+            <CloseOutlined onClick={() => deleteCart(id)}/>
           </div>      
        </section>
       </>
