e3204ebcdc56ceab1e3f55728bc1b21c3b57f7f6
Svetlana Govyazina <govyazina@gmail.com>
2023-06-22 16:03:09 +0300

    styles for cards fixed

--- online_store/src/components/ProductCard/ProductCard.jsx
+++ online_store/src/components/ProductCard/ProductCard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, Space } from 'antd';
 import Meta from 'antd/es/card/Meta';
 import { useNavigate } from 'react-router-dom';
+import Title from 'antd/es/typography/Title';
+import Text from 'antd/es/typography/Text';
 import styles from './productcard.module.scss';
 import AddToCartBtn from '../AddToCartBtn/AddToCartBtn';
 
@@ -19,19 +21,32 @@ export default function ProductCard({ product }) {
         height: 620,
         textAlign: 'center',
       }}
-      cover={<img alt={product.name_title} src={product.photos[0]} height={400} />}
+      cover={<img alt={product.name_title} src={product.photos[0]} height={350} />}
     >
       <Space
         className={styles.product_card}
       >
-        <Meta
-          title={product.name_title}
-          description={product.description.ingredients}
-        />
-        <Meta title={`${product.price} €`} />
-        <Space>
-          <AddToCartBtn product={product} />
-        </Space>
+        <Title
+          level={5}
+          className={styles.product_card__title}
+        >
+          {product.name_title}
+        </Title>
+        <Text
+          type="secondary"
+        >
+          {product.description.ingredients}
+        </Text>
+        <div>
+          <Meta title={`${product.price} €`} />
+          <Space
+            style={{
+              marginTop: '1rem',
+            }}
+          >
+            <AddToCartBtn product={product} />
+          </Space>
+        </div>
       </Space>
     </Card>
   );
